a4959f4bc3ba52e17a15af2e146662d468be6e22
user.name like: "Alex Nogueira <user.email like: "tec.alexnogueira@gmail.com>
2018-12-16 20:38:57 -0300

    Adcionando eventos nos btn, com addEventListnerAll

--- scripts/controller/CalcController.js
+++ scripts/controller/CalcController.js
@@ -8,6 +8,8 @@ class CalcController
             this._timeEl = document.querySelector("#hora");
             this._currentDate;
             this.initialize();
+            this.initButtonsEvents();
+           // this.addEventListenerAll();
         }
 
         initialize()
@@ -21,6 +23,32 @@ class CalcController
                 }, 1000);
 
             }
+
+        addEventListenerAll(element, events, fn)
+            {
+                events.split(' ').forEach(event =>
+                    {
+                        element.addEventListener(event, fn, false);
+                    });
+            }    
+
+            initButtonsEvents()
+                {
+                    let buttons = document.querySelectorAll("#buttons > g, #parts > g")
+
+                    buttons.forEach((btn, index) =>
+                        {
+                            this.addEventListenerAll(btn, "click drag", e => 
+                            {
+                                console.log(btn.className.baseVal.replace("btn-", ""));
+                            });
+
+                            this.addEventListenerAll(btn, "mouseover mouseup mousedown", e =>
+                            {
+                                btn.style.cursor = "pointer";
+                            });
+                        }
+                )};
 
         setDisplayDataTime()
             {
